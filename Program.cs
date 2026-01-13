59c242b08891ae73836ea740f742172cc88c6ebe
FvlSky <mariahannavb@gmail.com>
2026-01-13 10:44:44 +0800

    feat: Added score tracking feature

--- Program.cs
+++ Program.cs
@@ -4,6 +4,15 @@ namespace RockPaperScissors
 {
     class Program
     {
+        static int userScore = 0;
+        static int computerScore = 0;
+
+        static void PrintScore() 
+        {
+            Console.WriteLine($"\nYour Score:  {userScore}");
+            Console.WriteLine($"Computer's Score:  {computerScore}");
+        }
+
         static void Main(string[] args)
         {
             bool playAgain = true;
@@ -19,21 +28,15 @@ namespace RockPaperScissors
                 string userChoice = Console.ReadLine().ToUpper();
 
                 Random random = new Random();
-                int randomNum = random.Next(1, 4); 
+                int randomNum = random.Next(1, 4);
 
                 string computerChoice = "";
 
                 switch (randomNum)
                 {
-                    case 1:
-                        computerChoice = "ROCK";
-                        break;
-                    case 2:
-                        computerChoice = "PAPER";
-                        break;
-                    case 3:
-                        computerChoice = "SCISSORS";
-                        break;
+                    case 1: computerChoice = "ROCK"; break;
+                    case 2: computerChoice = "PAPER"; break;
+                    case 3: computerChoice = "SCISSORS"; break;
                 }
 
                 Console.WriteLine("Computer chose: " + computerChoice);
@@ -45,28 +48,48 @@ namespace RockPaperScissors
                 else if (userChoice == "ROCK")
                 {
                     if (computerChoice == "PAPER")
+                    {
                         Console.WriteLine("You LOSE! Paper covers Rock.");
+                        computerScore++; 
+                    }
                     else
+                    {
                         Console.WriteLine("You WIN! Rock smashes Scissors.");
+                        userScore++; 
+                    }
                 }
                 else if (userChoice == "PAPER")
                 {
                     if (computerChoice == "SCISSORS")
+                    {
                         Console.WriteLine("You LOSE! Scissors cut Paper.");
+                        computerScore++;
+                    }
                     else
+                    {
                         Console.WriteLine("You WIN! Paper covers Rock.");
+                        userScore++;
+                    }
                 }
                 else if (userChoice == "SCISSORS")
                 {
                     if (computerChoice == "ROCK")
+                    {
                         Console.WriteLine("You LOSE! Rock smashes Scissors.");
+                        computerScore++;
+                    }
                     else
+                    {
                         Console.WriteLine("You WIN! Scissors cut Paper.");
+                        userScore++;
+                    }
                 }
                 else
                 {
                     Console.WriteLine("Invalid entry! Please check your spelling.");
                 }
+
+                PrintScore(); 
 
                 Console.WriteLine("\nDo you want to play again? (Y/N):");
                 string answer = Console.ReadLine().ToUpper();
